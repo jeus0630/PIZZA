79ad184b4a903402d08d63b79ca3a631456e440a
Jewoo <jeus0630@designfever.com>
2022-03-14 01:56:29 +0900

    Docs: PizzaCard.tsx

--- components/PizzaList.tsx
+++ components/PizzaList.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import styles from "../styles/PizzaList.module.scss";
+import PizzaCard from './PizzaCard';
 
 type Props = {}
 
-export default function PizzaList({}: Props) {
-  return (
-    <div>PizzaList</div>
-  )
+export default function PizzaList({ }: Props) {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+            <p className={styles.desc}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repellendus culpa nobis. Dolorum, illum deleniti? Sed aperiam iure excepturi, dicta eligendi vitae fugit praesentium voluptatem voluptate, libero, sint vel dolore.
+            </p>
+            <div className={styles.wrapper}>
+                <PizzaCard></PizzaCard>
+                <PizzaCard></PizzaCard>
+                <PizzaCard></PizzaCard>
+                <PizzaCard></PizzaCard>
+                <PizzaCard></PizzaCard>
+                <PizzaCard></PizzaCard>
+            </div>
+        </div>
+    )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Featrued from '../components/Featrued'
-import styles from '../styles/Home.module.css'
+import PizzaList from '../components/PizzaList'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Pizza Restaurant in Newyork</title>
         <meta name="description" content="Best Pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Featrued></Featrued>
-
+      <PizzaList></PizzaList>
     </div>
   )
 }
