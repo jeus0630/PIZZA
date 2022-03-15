3bb99967b57fd643e3f60d2df58689fb9c0a2036
Jewoo <jeus0630@designfever.com>
2022-03-16 04:25:48 +0900

    Update: Navbar.tsx
    Redux toolkit with Link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,27 @@
 import styles from "../styles/Navbar.module.scss";
 import Image from 'next/image'
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 type Props = {}
 
 export default function Navbar({ }: Props) {
+    const state = useSelector((state: RootState) => state.cart);
+
     return (
         <div className={styles.container}>
-            <div className={styles.item}>
-                <div className={styles.callButton}>
-                    <Image src="/img/telephone.png" alt="" width="32" height="32"></Image>
+            <Link href={"/"}>
+                <div className={styles.item} style={{ cursor: 'pointer' }}>
+                    <div className={styles.callButton}>
+                        <Image src="/img/telephone.png" alt="" width="32" height="32"></Image>
+                    </div>
+                    <div className={styles.texts}>
+                        <div className={styles.text}>ORDER NOW!</div>
+                        <div className={styles.text}>012 345 678</div>
+                    </div>
                 </div>
-                <div className={styles.texts}>
-                    <div className={styles.text}>ORDER NOW!</div>
-                    <div className={styles.text}>012 345 678</div>
-                </div>
-            </div>
+            </Link>
             <div className={styles.item}>
                 <ul className={styles.list}>
                     <li className={styles.listItem}>Homepage</li>
@@ -27,10 +34,12 @@ export default function Navbar({ }: Props) {
                 </ul>
             </div>
             <div className={styles.item}>
-                <div className={styles.cart}>
-                    <Image src="/img/cart.png" alt="" width="30px" height="30px"></Image>
-                    <div className={styles.counter}>2</div>
-                </div>
+                <Link href={"/cart"}>
+                    <div className={styles.cart} style={{ cursor: 'pointer' }}>
+                        <Image src="/img/cart.png" alt="" width="30px" height="30px"></Image>
+                        <div className={styles.counter}>{state.total}</div>
+                    </div>
+                </Link>
             </div>
         </div>
     )
